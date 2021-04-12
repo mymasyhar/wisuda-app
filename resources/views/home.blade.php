307ba84b2dcad0200d9581b7077e5bdaf5d3d4d1
mymasyhar <mymasyhar@gmail.com>
2021-04-12 15:36:33 +0700

    fixing home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,19 @@
     <div class="panel-body bg-primary" style="margin-top: 40px; height:90vh; width:100vw; position: fixed;">
         <div style="width: 90vh; margin: 15% 25%; background-color:transparent" class="text-center">
             <div class="text-center">
-                <h3 class="text-light">SISTEM INFORMASI PENDAFTARAN WISUDA</h3>
-                <h3 class="text-light">DAN</h3>
-                <h3 class="text-light">ARSIP ALUMNI</h3>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3 class="text-light">SISTEM INFORMASI PENDAFTARAN WISUDA</h3>
+                        <h3 class="text-light">DAN</h3>
+                        <h3 class="text-light">ARSIP ALUMNI</h3>
+                    </div>
+                    <br>
+                    <div class="col-md-6" style="margin: 5% 25%">
+                        <a href="{{ route('login') }}" class="btn btn-warning btn-lg btn-block">LOGIN</a>
+                    </div>
+                </div>
+
             </div>
-            <hr>
-            <a href="{{ route('login') }}" class="btn btn-warning">LOGIN</a>
         </div>
     </div>
 </section>
